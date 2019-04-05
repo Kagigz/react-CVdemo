beff7e0db2136749185453f4828d8da882b145bb
Katia Gil Guzman <kagilguz@microsoft.com>
2019-04-05 19:20:27 +0200

    computer vision started

--- src/components/computervision.js
+++ src/components/computervision.js
@@ -4,6 +4,10 @@ import UrlUpload from './urlupload';
 import ImageView from './imageView';
 import Info from './info';
 
+const request = require('request');
+const subscriptionKey = '';
+const uriBase = 'https://westeurope.api.cognitive.microsoft.com/vision/v2.0/analyze';
+
 class ComputerVision extends React.Component{
 
     
@@ -15,15 +19,49 @@ class ComputerVision extends React.Component{
         };
          
       }
+
+    makeRequest = (imageURL) => {
+
+        const params = {
+            'visualFeatures': 'Categories,Description,Tags',
+            'details': '',
+            'language': 'en'
+        };
+        
+        const options = {
+            uri: uriBase,
+            qs: params,
+            body: '{"url": ' + '"' + imageURL + '"}',
+            headers: {
+                'Content-Type': 'application/json',
+                'Ocp-Apim-Subscription-Key' : subscriptionKey
+            }
+        };
+        
+        request.post(options, (error, response, body) => {
+          if (error) {
+            console.log('Error: ', error);
+            return;
+          }
+          let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+          console.log('JSON Response\n');
+          console.log(jsonResponse);
+        });
+
+    }
     
     handleSubmit = (files, allFiles) => {
         URL.revokeObjectURL(this.state.imgURL);
-        this.setState({imgURL:URL.createObjectURL(files[0].file)});
+        let newImg = URL.createObjectURL(files[0].file);
+        this.setState({imgURL:newImg});
+        console.log(newImg);
+        this.makeRequest(newImg);
         allFiles.forEach(f => f.remove())
     }
 
     handleOK = (url) => {
         this.setState({imgURL:url});
+        this.makeRequest(url);
     }
 
     render(){
